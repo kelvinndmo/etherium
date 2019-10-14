c2d6c1159723d20b433f3d9487517631983740da
novak <kelvin.onkundi@andela.com>
2019-10-14 03:10:10 +0300

    added angular cli

--- server/app.js
+++ server/app.js
@@ -4,9 +4,9 @@ const rpcURL = 'https://mainnet.infura.io/v3/938a148413fe4ac681dd510bba40d2f5';
 
 const web3 = new Web3(rpcURL);
 const express = require('express');
-var cors = require('cors');
+
 const app = express();
-app.use(cors());
+
 const port = process.env.PORT || 3000;
 
 const account = '0x90e63c3d53E0Ea496845b7a03ec7548B70014A91';
